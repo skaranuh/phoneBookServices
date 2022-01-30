f74221d8f4eeb370d72c348edeb05fde0a2c9191
skaranuh <selcuk.karanuh@gmail.com>
2022-01-30 21:43:00 +0300

    add composite index on contact info

---
+++ src/PhoneBook.Api.DataContext/Migrations/20220130183848_add-composite-index-on-contact-info.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhoneBook.Api.DataContext.Migrations
+{
+    public partial class addcompositeindexoncontactinfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactInfos_ContactInfoType_ContactPersonId_Value",
+                table: "ContactInfos",
+                columns: new[] { "ContactInfoType", "ContactPersonId", "Value" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ContactInfos_ContactInfoType_ContactPersonId_Value",
+                table: "ContactInfos");
+        }
+    }
+}
