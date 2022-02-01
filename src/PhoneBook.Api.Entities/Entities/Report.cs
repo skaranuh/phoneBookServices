e22d03f99363bea6039e8b87f5e445b11b7a7720
skaranuh <selcuk.karanuh@gmail.com>
2022-02-01 13:58:03 +0300

    addd "get report data" controller method

---
+++ src/PhoneBook.Api.Entities/Entities/Report.cs
@@ -0,0 +1,9 @@
+namespace PhoneBook.Api.Entities.Entities{
+
+    public class Report
+    {
+        public int PersonsCount { get; set; }
+        public int PhoneNumbersCount { get; set; }
+        public string Location { get; set; }
+    }
+}
