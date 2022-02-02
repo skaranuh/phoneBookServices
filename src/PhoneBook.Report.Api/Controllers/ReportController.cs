0d1397a9ec4ffebebdeb3c0f87b766c19a0b316a
skaranuh <selcuk.karanuh@gmail.com>
2022-02-02 06:55:15 +0300

    add report-microservice projects

---
+++ src/PhoneBook.Report.Api/Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PhoneBook.Report.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        
+        public ReportController()
+        {
+        
+        }
+              
+    }
+}
